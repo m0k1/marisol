c9d51b0bb6cc2a4975d074d066b61873a06fc797
Moki Ding <moki.forum@gmail.com>
2016-08-24 07:18:47 +0200

    Update 404.php

--- app/404.php
+++ app/404.php
@@ -1,1 +1,3 @@
-Page not found. (404) <a href="http://villa-marisol.me">CLICK HERE</a> to go back.
+Page not found. (404) <a href="http://villa-marisol.me">CLICK HERE</a> to go back.<br>
+<?php echo $get1; ?><br>
+<?php echo $get2; ?><br>
